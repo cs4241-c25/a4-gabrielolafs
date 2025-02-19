c7b1cd0177ecbb3a1656c751863b9e64bd3eec7e
Gabriel_O <55938114+gabrielolafs@users.noreply.github.com>
2025-02-18 22:46:35 -0500

    fixed signout

--- src/components/SignedIn.jsx
+++ src/components/SignedIn.jsx
@@ -29,7 +29,7 @@ const SignedIn = () => {
 
     return (
         <div className="container text-center mt-5">
-            <h1>Signed in as {username}.</h1>
+            <h1>Signed in as {username}</h1>
             <a href="#" onClick={handleSignOut} style={{ textDecoration: 'underline', cursor: 'pointer' }}>
                 Sign Out
             </a>
